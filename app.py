39f6936f3e6a81937906ac6d51cb408cc05e1042
Cleber-git <cleber192875@gmail.com>
2024-07-02 19:44:33 -0300

    Primeira api em python usando flask

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return "Bem vindo à sua primeira api com flask!"
+
+@app.route('/data')
+def getData():
+    data = {
+    'nome': 'Cleber',
+    'idade': 22,
+    'cidade': 'São Paulo'
+    }
+    data1 = {
+    'nome': 'Cleber',
+    'idade': 22,
+    'cidade': 'São Paulo'
+    }
+    data2 = {
+    'nome': 'Cleber',
+    'idade': 22,
+    'cidade': 'São Paulo'
+    }
+    data3 = {
+    'nome': 'Cleber',
+    'idade': 22,
+    'cidade': 'São Paulo'
+    }
+    data4 = {
+    'nome': 'Cleber',
+    'idade': 22,
+    'cidade': 'São Paulo'
+    }
+    j = {'data0': data,
+         'data1': data1,
+         'data2': data2,
+         'data3': data3,
+         'data4': data4
+         }
+    return jsonify(j)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True)
